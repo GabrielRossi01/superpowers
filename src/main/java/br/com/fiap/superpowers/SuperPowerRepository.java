cce891d536f2d8554485e3bed8e838f0bb0b4ac9
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-26 08:31:32 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/superpowers/SuperPowerRepository.java
@@ -0,0 +1,6 @@
+package br.com.fiap.superpowers;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SuperPowerRepository extends JpaRepository<Long, SuperPower> {
+}
